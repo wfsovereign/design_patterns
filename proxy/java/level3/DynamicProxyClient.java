4379c723eb2c74eddf3ddb9b6e87816d0fa13a48
wfsovereign <never_fall@sohu.com>
2015-03-26 10:55:04 +0800

    add my comprehension

--- proxy/java/level3/DynamicProxyClient.java
+++ proxy/java/level3/DynamicProxyClient.java
@@ -18,7 +18,14 @@ public class DynamicProxyClient {
         InvocationHandler handler = new DynamicProxy(realSubject);
 
         AbstractSubject proxy = (AbstractSubject) Proxy.newProxyInstance(loader, interfaces, handler);
-
+        /*
+        * 返回的是Proxy的Class类
+        * return sm != null && Proxy.ProxyAccessHelper.needsNewInstanceCheck(cl)?AccessController
+        * .doPrivileged(new PrivilegedAction() {
+        * public Object run() {
+        * return Proxy.newInstance(e, h);
+        * }
+        * */
 
         System.out.println(proxy instanceof Proxy);
         System.out.println(proxy.getClass().getName());
@@ -50,5 +57,23 @@ public class DynamicProxyClient {
 
         System.out.println("\n\n" + "运行结果为：");
         proxy.request();
-    }
+        /*
+        * before ----------public abstract void level3.AbstractSubject.request()
+        * real subject's request()........
+        * after ----------public abstract void level3.AbstractSubject.request()
+        * */
+
+        /*为什么调用proxy的request（）方法就能调用DynamicProxy中的invoke（）方法，
+        *因为proxy是属于Proxy类的，因为他的类是com.sun.proxy.$Proxy0，是继承了Proxy类的，
+        *而在$Proxy0中重写了request（）方法，并且调用了父类中h的invoke方法，而在父类Proxy中，
+        *h是这样定义的
+          *     protected InvocationHandler h;
+          *     也就是说调用的是InvocationHandler.invoke（）方法，
+          *     而DynamicProxy又实现了InvocationHandler接口的invoke（）方法，
+          *     因此，最后调用的便是DynamicProxy中的invoke（）方法。
+          *     这也就是java内置的动态代理实现机制。
+          *     */
+
+
+     }
 }
